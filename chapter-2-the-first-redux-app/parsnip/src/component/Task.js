6d817e192038e3953a78612bdda1c8ad8bf35b41
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-10-31 02:28:48 +0800

    using react-redux and redux

--- chapter-2-the-first-redux-app/parsnip/src/component/Task.js
+++ chapter-2-the-first-redux-app/parsnip/src/component/Task.js
@@ -1,16 +1,27 @@
 import React from 'react'
 
+const TASK_STATUES = ['Unstarted', 'In Progress', 'Complete']
 //无状态组件，只接收props
 const Task = props => {
     return (
         <div className="task">
             <div className="task-header">
                 <div>{props.task.title}</div>
+                <select value={props.task.status} onChange={onStatusChange}>
+                    {
+                        TASK_STATUES.map(status => (
+                            <option key={status} value={status}>{status}</option>
+                        ))
+                    }
+                </select>
             </div>
             <hr />
             <div className="task-body">{props.task.description}</div>
         </div>
     )
+    function onStatusChange(e) {
+        props.onStatusChange(props.task.id, e.target.value)
+    }
 }
 
 export default Task
--- chapter-2-the-first-redux-app/parsnip/src/component/TasksPage.js
+++ chapter-2-the-first-redux-app/parsnip/src/component/TasksPage.js
@@ -5,17 +5,93 @@ const TASK_STATUES = ['Unstarted', 'In Progress', 'Complete']
 
 //需要处理本地状态时要用ES6类
 class TasksPage extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            showNewCardForm: false,
+            title: '',
+            description: ''
+        }
+    }
+
+    onTitleChange = e => {
+        this.setState({ title: e.target.value })
+    }
+
+    onDescriptionChange = e => {
+        this.setState({ description: e.target.value })
+    }
+
+    resetForm = () => {
+        this.setState({
+            showNewCardForm: false,
+            title: '',
+            description: ''
+        })
+    }
+
+    onCreateTask = e => {
+        e.preventDefault()
+        this.props.onCreateTask({
+            title: this.state.title,
+            description: this.state.description
+        })
+        this.resetForm()
+    }
+
+    toggleForm = () => {
+        this.setState({
+            showNewCardForm: !this.state.showNewCardForm
+        })
+    }
+
     renderTaskLists = () => {
         const { tasks } = this.props
         return TASK_STATUES.map(status => {
             const statusTasks = tasks.filter(task => task.status === status)
-            return <TaskList key={status} status={status} tasks={statusTasks} />
+            return (<TaskList
+                key={status}
+                status={status}
+                tasks={statusTasks}
+                onStatusChange={this.props.onStatusChange}
+            />
+            )
         })
     }
 
     render() {
         return (
-            <div className="tasks">
+            <div className="task-list">
+                <div className='task-list-header'>
+                    <button
+                        className='button button-default'
+                        onClick={this.toggleForm}>
+                        + New Task
+                        </button>
+                </div>
+                {this.state.showNewCardForm && (
+                    <form className='task-list-form' onSubmit={this.onCreateTask}>
+                        <input
+                            className='full-width-input'
+                            onChange={this.onTitleChange}
+                            value={this.state.title}
+                            placeholder="title"
+                        />
+                        <input
+                            className='full-width-input'
+                            onChange={this.onDescriptionChange}
+                            value={this.state.description}
+                            placeholder="description"
+                        />
+                        <button
+                            className="button"
+                            type="submit"
+                        >
+                            Save
+                        </button>
+                    </form>
+                )}
                 <div className="task-lists">
                     {this.renderTaskLists()}
                 </div>
